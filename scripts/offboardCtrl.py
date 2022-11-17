dcc3845486bb21eda3f75be7c4804e0d03a9fd5d
Scientist90s <yshethwala@gmail.com>
2022-11-17 10:32:38 -0700

    multiple UAV's added

--- scripts/offboardCtrl.py
+++ scripts/offboardCtrl.py
@@ -6,11 +6,12 @@
 import math
 import rospy
 import numpy
-from mavros_msgs.msg import State
 from std_msgs.msg import String
 from geometry_msgs.msg import PoseStamped, Point, Quaternion
-from mavros_msgs.srv import CommandBool, SetMode
+from mavros_msgs.srv import CommandBool, CommandBoolRequest, SetMode, SetModeRequest
+from mavros_msgs.msg import State
 from tf.transformations import quaternion_from_euler, euler_from_quaternion
+from time import sleep
 
 # Offboard control class
 class OffbPosCtl():
@@ -18,7 +19,7 @@ class OffbPosCtl():
         # defined counters
         self.distThreshold = 0.2
         self.angThreshold = 0.1
-        self.nofagents = 4
+        self.nofagents = 1
         # some more definitions
         self.pose_pub = []
         self.drone_pose_subscriber = []
@@ -59,9 +60,9 @@ class OffbPosCtl():
             self.arm.append(False)
             
         # setting ros rate
-        self.rate = rospy.Rate(10)  # Hz
+        self.rate = rospy.Rate(20)  # Hz
         self.rate.sleep()
-        print("-----Starting control-----")
+        rospy.loginfo("-----Starting control-----")
         self.controller()
         
     # callback function to store drone pose
@@ -70,45 +71,71 @@ class OffbPosCtl():
         
     # callback function to store drone state
     def drone_state_cb(self, msg, agent):
-        self.curr_drone_state[agent] = msg.mode
+        self.curr_drone_state[agent] = msg
         
     # Function to set offboard mode
     def set_offboard_mode(self, agent):
         rospy.wait_for_service('/uav'+str(agent)+'/mavros/set_mode')
+        offb_set_mode = SetModeRequest()
+        offb_set_mode.custom_mode = 'OFFBOARD'
         try:
-            self.flightModeService[agent](custom_mode='OFFBOARD')
+            if(self.flightModeService[agent].call(offb_set_mode).mode_sent == True):
+                rospy.loginfo(f"{agent} agent's Offboard enabled")
         except rospy.ServiceException as e:
             print("service set_mode call failed: %s. OFFBOARD Mode could not be set. Check that GPS is enabled" % e)
+            
+    def set_hold_mode(self, agent):
+        rospy.wait_for_service('/uav'+str(agent)+'/mavros/set_mode')
+        hold_set_mode = SetModeRequest()
+        hold_set_mode.custom_mode = 'HOLD'
+        try:
+            if(self.flightModeService[agent].call(hold_set_mode).mode_sent == True):
+                rospy.loginfo(f"{agent} agent's hold enabled")
+        except rospy.ServiceException as e:
+            print("service set_mode call failed: %s. HOLD Mode could not be set. Check that GPS is enabled" % e)
 
     # Function to arm the drone
     def set_arm(self, agent):
         rospy.wait_for_service('/uav'+str(agent)+'/mavros/cmd/arming')
+        arm_cmd = CommandBoolRequest()
+        arm_cmd.value = True
         try:
-            self.armService[agent](True)
-            self.arm = True
+            if(self.armService[agent].call(arm_cmd).success == True):
+                rospy.loginfo(f"{agent} agent's Armed")
         except rospy.ServiceException as e:
             print("Service arm call failed: %s" % e)
             
     # Function to takeoff
     def takeoff(self):
         for i in range(self.nofagents):
-            if self.curr_drone_state[i] == "OFFBOARD" and self.arm[i] == True:
+            if self.curr_drone_state[i].mode == "OFFBOARD" and self.curr_drone_state[i].arm == True:
                 self.isReadyToFly[i] = True
             else:
-                self.set_offboard_mode(i)
-                self.set_arm(i)
-        self.mode = "ASCEND"
+                if self.curr_drone_state[i].mode != "OFFBOARD":
+                    self.set_offboard_mode(i)
+                if self.curr_drone_state[i].armed != True:
+                    self.set_arm(i)
+            sleep(1)
+        self.mode = "YASH"
+        
+    def ascend(self):
+        for i in range(self.nofagents):
+            self.des_pose[i].pose.position.z = 1
+            while self.mode[i] == "ASCEND" and not rospy.is_shutdown():
+                self.pose_pub[i].publish(self.des_pose[i])
+                if self.curr_drone_pose[i].pose.position.z >= 0.95:
+                    self.set_hold_mode(i)
         
     def controller(self):
         """ A state machine developed to have UAV states """
         while not rospy.is_shutdown():
             # control your UAV states and functionalities here...
             if self.mode == "TAKEOFF":
-                print("-----Being Ready to TakeOff-----")
+                rospy.loginfo("-----Being Ready to TakeOff-----")
                 self.takeoff()
-            # if self.mode == "ASCEND":
-            #     print("-----Ascending!-----")
-            #     self.ascend()
+            if self.mode == "ASCEND":
+                rospy.loginfo("-----Ascending!-----")
+                self.ascend()
             # if self.mode == "BELLY-FLOP":
             #     print("belly flop!")
             #     self.belly_flop()
